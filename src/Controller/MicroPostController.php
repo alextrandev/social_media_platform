70fd9b93d62f928e7ee3d1097d8137df687f9e51
Alex Tran <tranduclong092@gmail.com>
2024-06-24 20:48:59 +0300

    Feat: add 3 fake posts to data fixtures and dd the posts on page

--- src/Controller/MicroPostController.php
+++ src/Controller/MicroPostController.php
@@ -2,13 +2,18 @@
 
 namespace App\Controller;
 
+use App\Repository\MicroPostRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-class MicroPostController extends AbstractController {
+class MicroPostController extends AbstractController
+{
     #[Route('/micro-post', name: 'app_micro_post', priority: 1)]
-    public function index(): Response {
+    public function index(MicroPostRepository $posts): Response
+    {
+        dd($posts->findAll());
+
         return $this->render('micro_post/index.html.twig', [
             'controller_name' => 'MicroPostController',
         ]);
--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,6 +2,8 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\MicroPost;
+use DateTime;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -9,8 +11,23 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $microPost1 = new MicroPost();
+        $microPost1->setTitle('Welcome to Helsinki!');
+        $microPost1->setText('Welcome to Helsinki!');
+        $microPost1->setCreated(new DateTime());
+        $manager->persist($microPost1);
+
+        $microPost2 = new MicroPost();
+        $microPost2->setTitle('Welcome to the US!');
+        $microPost2->setText('Welcome to the US!');
+        $microPost2->setCreated(new DateTime());
+        $manager->persist($microPost2);
+
+        $microPost3 = new MicroPost();
+        $microPost3->setTitle('Welcome to Japan!');
+        $microPost3->setText('Welcome to Japan!');
+        $microPost3->setCreated(new DateTime());
+        $manager->persist($microPost3);
 
         $manager->flush();
     }
