5a6d666400e37edb7f86a7cdf3f63da3c754d752
Alex Tran <tranduclong092@gmail.com>
2024-06-30 13:27:16 +0300

    Feat: create a form template for adding new post

--- src/Controller/MicroPostController.php
+++ src/Controller/MicroPostController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Entity\MicroPost;
 use App\Repository\MicroPostRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -23,6 +24,21 @@ class MicroPostController extends AbstractController
     {
         return $this->render('micro_post/show.html.twig', [
             'post' => $post,
+        ]);
+    }
+
+    #[Route('micro-post/add', name: 'app_micro_post_add', priority: 2)]
+    public function add(): Response
+    {
+        $post = new MicroPost();
+        $form = $this->createFormBuilder($post)
+            ->add('title')
+            ->add('text')
+            ->add('submit', SubmitType::class, ['label' => 'Post'])
+            ->getForm();
+
+        return $this->render('micro_post/add.html.twig', [
+            'form' => $form,
         ]);
     }
 }
