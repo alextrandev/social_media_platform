0b9ab6d0373b009ea3226929c4767d382010b8ad
Alex Tran <tranduclong092@gmail.com>
2024-10-14 16:08:41 +0300

    Feat: test the one to one relation using hello controller

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -2,30 +2,52 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Entity\UserProfile;
+use App\Repository\UserProfileRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class HelloController extends AbstractController {
+class HelloController extends AbstractController
+{
     private array $messages = [
         ['message' => 'Hello', 'created' => '2024/04/01'],
         ['message' => 'Hi', 'created' => '2024/02/01'],
         ['message' => 'Bye!', 'created' => '2023/03/01']
     ];
 
-    #[Route('/{limit?3}', name: 'app_index')]
-    public function index(int $limit): Response {
+    #[Route('/', name: 'app_index')]
+    public function index(EntityManagerInterface $em, UserProfileRepository $profiles): Response
+    {
+        // // add user and profile
+        // $user = new User();
+        // $user->setEmail('email@email.com');
+        // $user->setPassword('12345678');
+
+        // $profile = new UserProfile();
+        // $profile->setUser($user);
+        // $em->persist($profile);
+        // $em->flush();
+
+        // // remove user and profile
+        // $profile = $profiles->find(1);
+        // $em->remove($profile);
+        // $em->flush();
+
         return $this->render(
             'hello/index.html.twig',
             [
                 'messages' => $this->messages,
-                'limit' => $limit
+                'limit' => 3
             ]
         );
     }
 
     #[Route('/messages/{id<\d+>}', name: 'app_show_one')]
-    public function showOne(int $id): Response {
+    public function showOne(int $id): Response
+    {
         return $this->render(
             'hello/show_one.html.twig',
             [
