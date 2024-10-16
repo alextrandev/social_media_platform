f746792fca9eb56dfcb2ac042defb527cc8b47ba
Alex Tran <tranduclong092@gmail.com>
2024-10-16 18:35:16 +0300

    Feat: create logout firewall rule and controller route

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -21,4 +21,8 @@ class LoginController extends AbstractController
             'error' => $error
         ]);
     }
+
+    #[Route('/logout', name: 'app_logout')]
+    public function logout() {}
+    // anonymous function here because symfony will automatically intercept it and handle the logic
 }
