54858ceee5ac0573ce322de5a4d80fb1a53a5f01
Alex Tran <tranduclong092@gmail.com>
2024-10-16 16:07:32 +0300

    Feat: load 2 fake users using fixture

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -3,14 +3,40 @@
 namespace App\DataFixtures;
 
 use App\Entity\MicroPost;
+use App\Entity\User;
 use DateTime;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class AppFixtures extends Fixture
 {
+    public function __construct(
+        private UserPasswordHasherInterface $userPasswordHasher
+    ) {}
+
     public function load(ObjectManager $manager): void
     {
+        $user1 = new User();
+        $user1->setEmail('test@test.com');
+        $user1->setPassword(
+            $this->userPasswordHasher->hashPassword(
+                $user1,
+                'password'
+            )
+        );
+        $manager->persist($user1);
+
+        $user2 = new User();
+        $user2->setEmail('alex@test.com');
+        $user2->setPassword(
+            $this->userPasswordHasher->hashPassword(
+                $user2,
+                'password'
+            )
+        );
+        $manager->persist($user2);
+
         $microPost1 = new MicroPost();
         $microPost1->setTitle('Welcome to Helsinki!');
         $microPost1->setText('Welcome to Helsinki!');
