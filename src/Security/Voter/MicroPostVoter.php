1a3b6584dbb0799f684debb708a6109eb3295df0
Alex Tran <tranduclong092@gmail.com>
2024-10-18 15:17:06 +0300

    Feat: define voter roles and permissions

--- src/Security/Voter/MicroPostVoter.php
+++ src/Security/Voter/MicroPostVoter.php
@@ -2,43 +2,63 @@
 
 namespace App\Security\Voter;
 
-use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
-use Symfony\Component\Security\Core\Authorization\Voter\Voter;
+use App\Entity\User;
+use App\Entity\MicroPost;
+use Symfony\Bundle\SecurityBundle\Security;
 use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Security\Core\Authorization\Voter\Voter;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 
 class MicroPostVoter extends Voter
 {
-    public const EDIT = 'POST_EDIT';
-    public const VIEW = 'POST_VIEW';
+    public function __construct(
+        private Security $security
+    ) {}
 
     protected function supports(string $attribute, mixed $subject): bool
     {
         // replace with your own logic
         // https://symfony.com/doc/current/security/voters.html
-        return in_array($attribute, [self::EDIT, self::VIEW])
+        return in_array($attribute, [MicroPost::EDIT, MicroPost::VIEW])
             && $subject instanceof \App\Entity\MicroPost;
     }
 
+    /**
+     * Function to decide permission on view and edit post
+     *
+     * @param MicroPost $subject
+     * @param string $attribute
+     * @param mixed $subject
+     * @param TokenInterface $token
+     * @return boolean
+     */
     protected function voteOnAttribute(string $attribute, mixed $subject, TokenInterface $token): bool
     {
+        /** @var User $user */
         $user = $token->getUser();
 
         // if the user is anonymous, do not grant access
-        if (!$user instanceof UserInterface) {
-            return false;
+        // if (!$user instanceof UserInterface) {
+        //     return false;
+        // }
+
+        $isAuth = $user instanceof UserInterface;
+
+        if ($this->security->isGranted('ROLE_ADMIN')) {
+            return true;
         }
 
         // ... (check conditions and return true to grant permission) ...
         switch ($attribute) {
-            case self::EDIT:
-                // logic to determine if the user can EDIT
-                // return true or false
-                break;
+            case MicroPost::EDIT:
+                return $isAuth
+                    && (
+                        ($subject->getAuthor()->getId() === $user->getId()) ||
+                        $this->security->isGranted('ROLE_EDITOR')
+                    );
 
-            case self::VIEW:
-                // logic to determine if the user can VIEW
-                // return true or false
-                break;
+            case MicroPost::VIEW:
+                return true;
         }
 
         return false;
